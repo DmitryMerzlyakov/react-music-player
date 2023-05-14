e8f668aa4daec045caf25fefac7f2fa65a604d6b
Dmitry <jagotti@yandex.ru>
2023-05-14 19:33:16 +0300

    add audio and track status

--- src/Main/Footer/mainFooter.jsx
+++ src/Main/Footer/mainFooter.jsx
@@ -5,13 +5,48 @@ import s from './css/mainFooter.module.css'
 
 const Footer = () => {
 
-    const [loading, setLoading] = useState(true);
-    const playRef = useRef(null);
+    const [loading, setLoading] = useState(false);
+    const [play, setPlay] = useState(false);
+    const [currentTime, setCurrentTime] = useState(0)
+    const [duration, setDuration] = useState(0)
 
-    const hendlePlay = () => {
-        playRef.current.play()
-        console.log(playRef);
+    const playRef = useRef(null);
+    const progressSong = useRef();
+    const animationRef = useRef(); 
+
+    const togglePlay = () => {
+        const prevValue = play;
+        console.log(currentTime);
+        console.log(duration);
+        setPlay(!prevValue);
+        if (!prevValue) {
+        playRef.current.play();
+        animationRef.current = requestAnimationFrame(whilePlaying)
+        } else {
+        playRef.current.pause();
+        cancelAnimationFrame(animationRef.current);
+        }
     }
+
+    const whilePlaying = () => {
+        progressSong.current.value = playRef.current.currentTime;
+        progressSong.current.style.setProperty('--seek-before-width', `${progressSong.current.value / playRef.current.duration * 100}%`)
+        setCurrentTime(progressSong.current.value);
+        animationRef.current = requestAnimationFrame(whilePlaying);
+    }
+
+    const changeRange = () => {
+        playRef.current.currentTime = progressSong.current.value;
+        progressSong.current.style.setProperty('--seek-before-width', `${progressSong.current.value / playRef.current.duration * 100}%`)
+        setCurrentTime(progressSong.current.value);
+    }
+
+    useEffect(() => {
+        const seconds = Math.floor(playRef.current.duration);
+        setDuration(seconds);
+        progressSong.current.max = seconds;
+    }, [playRef?.current?.loadedmetadata, playRef?.current?.readyState]);
+
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -22,8 +57,18 @@ const Footer = () => {
 
     return (
         <div className={s.bar}>
+            <audio
+                ref={playRef} 
+                src="../audio/Bobby_Marleni_-_Dropin.mp3">
+            </audio>
             <div className={s.content}>
-                    <div className={s.progress}></div>
+                    <input 
+                        type='range' 
+                        className={s.progress}
+                        defaultValue='0'
+                        ref={progressSong}
+                        onChange={changeRange}
+                    />
                     <div className={s.block}>
                         <div className={s.player}>
                         <div className={s.controls}>
@@ -32,30 +77,34 @@ const Footer = () => {
                                         <use xlinkHref={`${sprite}#icon-prev`}></use>
                                     </svg>
                                 </div>
-                            <div className={`${s.play} `}  onClick={hendlePlay}>
-                                    <audio
-                                    ref={playRef} 
-                                    src="../audio/Bobby_Marleni_-_Dropin.mp3"></audio>
+                            <div className={s.play} onClick={togglePlay}>
+                                {
+                                play ?  
+                                    <svg className={s.stopsvg} alt="play">
+                                        <use xlinkHref={`${sprite}#stop`}></use> 
+                                    </svg>
+                                    :
                                     <svg className={s.playsvg} alt="play">
-                                        <use xlinkHref={`${sprite}#icon-play`}></use>
+                                        <use xlinkHref={`${sprite}#icon-play`}></use> 
                                     </svg>
-                                </div>
-                                <div className={s.next}>
-                                    <svg className={s.nextsvg} alt="next">
-                                        <use xlinkHref={`${sprite}#icon-next`}></use>
-                                    </svg>
-                                </div>
-                                <div className={`${s.repeat}`}>
-                                    <svg className={s.repeatsvg} alt="repeat">
-                                        <use xlinkHref={`${sprite}#icon-repeat`}></use>
-                                    </svg>
-                                </div>
-                            <div className={`${s.shuffle} `}>
-                                    <svg className={s.shufflesvg} alt="shuffle">
-                                        <use xlinkHref={`${sprite}#icon-shuffle`}></use>
-                                    </svg>
-                                </div>
+                                }
                             </div>
+                            <div className={s.next}>
+                                <svg className={s.nextsvg} alt="next">
+                                    <use xlinkHref={`${sprite}#icon-next`}></use>
+                                </svg>
+                            </div>
+                            <div className={`${s.repeat}`}>
+                                <svg className={s.repeatsvg} alt="repeat">
+                                    <use xlinkHref={`${sprite}#icon-repeat`}></use>
+                                </svg>
+                            </div>
+                            <div className={`${s.shuffle} `}>
+                                <svg className={s.shufflesvg} alt="shuffle">
+                                    <use xlinkHref={`${sprite}#icon-shuffle`}></use>
+                                </svg>
+                            </div>
+                        </div>
                             
                         <div className={s.trackplay}>
                             {loading ? (
